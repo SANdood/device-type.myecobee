d12c0fcceab7ddde0b12f8a2709d1c171b95874f
Barry Burke <SANdood@users.noreply.github.com>
2015-05-23 14:06:25 -0400

    Latest updates from Yves

--- smartapps/ecobeeRemoteSensorsInit.groovy
+++ smartapps/ecobeeRemoteSensorsInit.groovy
@@ -64,41 +64,37 @@ preferences {
 
 
 def selectMotionSensors() {
-	def REMOTE_OCCUPANCY_TYPE='occupancy'
 
-	/* Get the list of remote sensors available 
+	/* Generate the list of all remote sensors available 
 	 */
-	ecobee.generateRemoteSensorEvents("", 'true')
-	def ecobeeSensors
+	ecobee.generateRemoteSensorEvents("", 'false')
     
-	String remoteSensorData=ecobee.currentRemoteSensorData.toString()
-	if (remoteSensorData != null) {    
-		ecobeeSensors = new JsonSlurper().parseText(remoteSensorData)
-	} else {
-		log.error("selectMotionSensors>sensorRemoteData is empty, exiting")
-		return        
-	}    
-	log.debug "selectMotionSensors>ecobeeSensors data = $ecobeeSensors"
 	def sensors = [: ]
 
-	if (ecobeeSensors?.size() > 0) {
+	/* Get only the list of all occupancy remote sensors available 
+	 */
 
-		for (i in 0..ecobeeSensors.size() - 1) {
+	def ecobeeSensors = ecobee.currentRemoteSensorOccData.toString().split(",,")
 
+	log.debug "selectMotionSensors>ecobeeSensors= $ecobeeSensors"
 
-			for (j in 0..ecobeeSensors[i].capability.size() - 1) {
+	if (ecobeeSensors.size() < 1) {
 
-				if (ecobeeSensors[i].capability[j].type == REMOTE_OCCUPANCY_TYPE) {
+		log.debug "selectMotionSensors>no values found"
+		return sensors
 
-					if (settings.trace) {
-						log.debug "selectMotionSensors>>looping at i=${i},found occupancy sensor ${ecobeeSensors[i].capability[j]} at j=${j}"
-					}
+	}
 
-					def dni = [app.id, ecobeeSensors[i].name, getMotionSensorChildName(), ecobeeSensors[i].id].join('.')
-					sensors[dni] = ecobeeSensors[i].name
-				}
-			}
-		}
+	for (i in 0..ecobeeSensors.size() - 1) {
+
+		def ecobeeSensorDetails = ecobeeSensors[i].split(',')
+		def ecobeeSensorId = ecobeeSensorDetails[0]
+		def ecobeeSensorName = ecobeeSensorDetails[1]
+		def ecobeeSensorType = ecobeeSensorDetails[2]
+
+		def dni = [app.id, ecobeeSensorName, getMotionSensorChildName(), ecobeeSensorId].join('.')
+
+		sensors[dni] = ecobeeSensorName
 
 	}
 
@@ -116,36 +112,31 @@ def selectMotionSensors() {
 
 
 def selectTempSensors() {
-	def ecobeeSensors
-	def REMOTE_TEMPERATURE_TYPE='temperature'
     
-    
-	String remoteSensorData=ecobee.currentRemoteSensorData.toString()
-	if (remoteSensorData != null) {    
-		ecobeeSensors = new JsonSlurper().parseText(remoteSensorData)
-	} else {
-		log.error("selectMotionSensors>sensorRemoteData is empty, exiting")
-		return        
-	}    
-	log.debug "selectTempSensors>ecobeeSensors data = $ecobeeSensors"
-
 	def sensors = [: ]
-	if (ecobeeSensors?.size() > 0) {
-		for (i in 0..ecobeeSensors.size() - 1) {
+	/* Get only the list of all temperature remote sensors available 
+	 */
+	def ecobeeSensors = ecobee.currentRemoteSensorTmpData.toString().split(",,")
 
-			for (j in 0..ecobeeSensors[i].capability.size() - 1) {
+	log.debug "selectTempSensors>ecobeeSensors= $ecobeeSensors"
 
-				if (ecobeeSensors[i].capability[j].type == REMOTE_TEMPERATURE_TYPE) {
+	if (ecobeeSensors.size() < 1) {
 
-					if (settings.trace) {
-						log.debug "selectTempSensors>>looping at i=${i},found temperature sensor ${ecobeeSensors[i].capability[j]} at j=${j}"
-					}
+		log.debug "selectTempSensors>no values found"
+		return sensors
+	}
 
-					def dni = [app.id, ecobeeSensors[i].name, getTempSensorChildName(), ecobeeSensors[i].id].join('.')
-					sensors[dni] = ecobeeSensors[i].name
-				}
-			}
-		}
+	for (i in 0..ecobeeSensors.size() - 1) {
+
+		def ecobeeSensorDetails = ecobeeSensors[i].split(',')
+		def ecobeeSensorId = ecobeeSensorDetails[0]
+		def ecobeeSensorName = ecobeeSensorDetails[1]
+		def ecobeeSensorType = ecobeeSensorDetails[2]
+
+
+		def dni = [app.id, ecobeeSensorName, getTempSensorChildName(), ecobeeSensorId].join('.')
+
+		sensors[dni] = ecobeeSensorName
 
 	}
 
@@ -305,10 +296,10 @@ def takeAction() {
 	log.trace "takeAction>about to call generateRemoteSensorEvents()"
 	ecobee.generateRemoteSensorEvents("", 'false')
 	updateMotionSensors()
-	updateTempSensors()
-    
+	updateTempSensors()    
+/*	
 	updateHumiditySensors()
-	
+*/
 	log.trace "takeAction>end"
 }
 
@@ -534,4 +525,3 @@ def getMotionSensorChildName() {
 def getTempSensorChildName() {
 	"Temperature Sensor"
 }
-
