d1efb13bf221e2d78ef879d04ce9534ce517264b
Barry Burke <SANdood@users.noreply.github.com>
2015-10-26 14:42:02 -0400

    Update to yracine 1.5.3 - Oct 24 2015

--- smartapps/ecobeeRemoteSensorsInit.groovy
+++ smartapps/ecobeeRemoteSensorsInit.groovy
@@ -31,7 +31,7 @@ preferences {
 	page(name: "selectThermostat", title: "Ecobee Thermostat", install: false, uninstall: true, nextPage: "selectMotionSensors") {
 		section("About") {
 			paragraph "ecobeeRemoteSensorsInit, the smartapp that creates individual ST sensors for your ecobee3's remote Sensors and polls them on a regular basis"
-			paragraph "Version 1.5" 
+			paragraph "Version 1.5.2" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -237,9 +237,13 @@ private def deleteSensors() {
 	}
 	log.trace("ecobeeRemoteSensorsInit>deleting ${delete.size()} MyEcobee's Motion Sensors")
 	delete.each {
-		deleteChildDevice(it.deviceNetworkId)
+		try {    
+			deleteChildDevice(it.deviceNetworkId)
+		} catch(e) {
+			log.error("ecobeeRemoteSensorsInit>exception $e while trying to delete Motion Sensor ${it.deviceNetworkId}")
+			send("ecobeeRemoteSensorsInit>exception $e while trying to delete Motion Sensor ${it.deviceNetworkId}")
+		}   
 	}
-
 
 	if (!tempSensors) {
 		log.debug "delete all Temp/Humidity Sensors"
@@ -253,7 +257,12 @@ private def deleteSensors() {
 	}
 	log.trace("ecobeeRemoteSensorsInit>deleting ${delete.size()} MyEcobee's Temp Sensors")
 	delete.each {
-		deleteChildDevice(it.deviceNetworkId)
+		try {
+			deleteChildDevice(it.deviceNetworkId)
+		} catch (e) {
+			log.error("ecobeeRemoteSensorsInit>exception $e while trying to delete Temp Sensor ${it.deviceNetworkId}")
+			send("ecobeeRemoteSensorsInit>exception $e while trying to delete Temp Sensor ${it.deviceNetworkId}")
+		}     
 	}
 
 }
